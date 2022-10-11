3d27f66ae94605ea8d55d72d3d4fdf14a9d953d5
More <2026869@cognizant.com>
2022-10-11 13:34:39 +0530

    new user

--- src/app/user/user-details/user-details.component.ts
+++ src/app/user/user-details/user-details.component.ts
@@ -1,75 +1,47 @@
-import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FlightinfoService } from 'src/app/_Services/flightinfo.service';
-
+import { CourseService } from 'src/app/_Services/course.service';
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.scss']
 })
 export class UserDetailsComponent implements OnInit {
-  userid: any = localStorage.getItem('User');
+  errorMessage: string = ""
   result: any = {}
-  errorMessage = ""
-  BookingList: any = []
-  uniqueSch: any = []
-  schdulDetails: any = []
-  isEmpty = true
-  ShowingDetails: any = []
-  constructor(private datePipe: DatePipe, private flightinfoService: FlightinfoService) { }
-
+  Isresult = false
+  coursename = "";
+  url = "";
+  visibile = false;
+  courseduration = 0;
+  coursedescription = "";
+  technology = "";
+  total_airlines = 0
+  isedit = false
+  airId = ""
+  constructor(private courseService: CourseService) { }
   ngOnInit(): void {
-    this.flightinfoService.ShowBookingbyUserId(this.userid)
+    this.loadAllCourse()
+  }
+  loadAllCourse() {
+    this.courseService.getCourse()
       .subscribe((res: any) => {
+        this.total_airlines = res.length;
+        if (this.total_airlines != 0) {
+          this.Isresult = true
+        }
         this.result = res;
-        if (res.length > 0) {
-          var distinct = new Set();
-          for (var i = 0; i < this.result.length; i++) {
-            distinct.add(this.result[i].schlId)
-          }
-          this.GetScheduleDetails(distinct)
-        }
-        else {
-          this.isEmpty = true
-        }
-        //this.TestBooking()
       },
         (err: any) => {
           console.log(err)
           this.errorMessage = err.message;
         })
   }
-  GetScheduleDetails(Schids: any) {
 
-    console.log("GetScheduleDetails")
-    for (var id of Schids) {
-      //count++
-      this.flightinfoService.ShowSchedulebyId(id)
-        .subscribe((res: any) => {
-          this.BookingDetailsForManange(res)
-        },
-          (err: any) => {
-            console.log(err)
-            this.errorMessage = err.message;
-          })
-    }
-  }
-  BookingDetailsForManange(res: any) {
-    var today: any = new Date()
-    today = this.datePipe.transform(today, 'yyyy-MM-dd')
-    var schdate: any = this.datePipe.transform(res.date, 'yyyy-MM-dd')
-    if (schdate < today) {
-      for (var i = 0; i < this.result.length; i++) {
-        if (this.result[i].schlId == res.id) {
-          this.ShowingDetails.push({
-            "bookingdetails": this.result[i],
-            "scheduleDetails": res
-          })
-        }
-      }
-      this.isEmpty = false
-    }
-  }
+
+
+  
+
+
 
 }
 
